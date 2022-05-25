62691fff560fe0e439c0458d372305d402cd6db8
kjboogie <trickxbooks@gmail.com>
2022-05-25 13:21:33 +0530

    ERC-20 Token specification completed. Delegated tokens with all test cases done

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -11,26 +11,54 @@ contract Token {
     uint256 public decimals = 18;   
     uint256 public totalSupply ; 
 
-    //Track Balances 
-    mapping(address => uint256) public balanceOf;
+    // Mapping
+    mapping(address => uint256) public balanceOf; //Track Balances 
+    //mapping to add amount in allowance 
+    mapping(address => mapping(address => uint256)) public allowance; //first mapping has the address who approves to spend on exchanges
+                                                                      // second mapping has all the address of the exchanges where it gets approval with the respective amount
    
    //Events
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner,address indexed spender,uint256 value);
+
+
+    //internal function to transfer tokens. Can't be called from blockchain
+    function _transfer(address _from, address _to, uint256 _value) internal returns (bool success){
+         // require funtion will not execute further statements if condition is false inside it
+        require( _to != address(0)); // Checking if reciever address is valid or not
+        balanceOf[_from] = balanceOf[msg.sender].sub(_value); //subtracting tokens from sender
+        balanceOf[_to] = balanceOf[_to].add(_value);  // adding tokens to reciever
+        //Calling the event
+       emit Transfer(_from,_to,_value);
+        return true;
+    }
 
    // Send tokens or transer function
    function transfer(address _to, uint256 _value ) public returns (bool success){
-       // require funtion will not execute further statements if condition is false inside it
-        require( _to != address(0)); // Checking if reciever address is valid or not
        require(balanceOf[msg.sender] >= _value);   //Checking if sender has desired amount of token to send or not
-       balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
-       balanceOf[_to] = balanceOf[_to].add(_value);
-       //Calling the event
-       emit Transfer(msg.sender,_to,_value);
+        _transfer(msg.sender,_to,_value);
+       return true;
+   }
+
+   //Approve Tokens to spend for ecxhange
+   function approve(address _spender,uint256 _value) public returns (bool Success){
+       require(_spender != address(0));
+       allowance[msg.sender][_spender]= _value;
+       emit Approval(msg.sender, _spender, _value);
+       return true;
+   }
+
+   //transfer function for exchange
+   function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+       require(_value <= balanceOf[_from]); // sending amount should be less than the total token sender has
+       require(_value <= allowance[_from][msg.sender]); // sending amount should be less than the approved amount
+       allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);  // reset the allowance
+       _transfer(_from, _to, _value);  //Calling _transfer function
        return true;
    }
 
     constructor() public {
-        totalSupply = 1000000 * (10 ** decimals);
-        balanceOf[msg.sender] = totalSupply;
+        totalSupply = 1000000 * (10 ** decimals);  // setting total supply wheneer smart contract is deployed
+        balanceOf[msg.sender] = totalSupply;  // adding total supply of token to deployer
     }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,4 +1,5 @@
 import { assert } from "chai"
+import { eq } from "lodash"
 import Web3 from "web3"
 import {tokens,EVM_REVERT} from './helper.js'
 
@@ -10,8 +11,8 @@ require('chai')
 const Token = artifacts.require('./Token')
 
 
-
-contract('Token', ()=>{
+//In parameters we can define the accounts
+contract('Token', (accounts)=>{
     const name = 'Boogiereum Token'
     const symbol = 'BOOGIE'
     const decimals = '18'
@@ -105,4 +106,95 @@ contract('Token', ()=>{
     })
 
     // Aproving tokens so that onyone/exchanges can send tokens from user's account
+    describe('approving tokens', async ()=> {
+        let result
+        let amount
+        beforeEach(async () => {
+            amount = tokens(100)
+            result = await token.approve(accounts[2],amount,{from: accounts[0]})  //account[0] approves the allowance for account[2] to send amount
+        }) 
+        describe('success', () =>{
+            it('allocate an allowance for delegated token spending on exchanges', async () =>{
+                const allowance = await token.allowance(accounts[0],accounts[2]) 
+                allowance.toString().should.equal(amount.toString()) // checking if the amount of tokens are added to the allowance
+            })
+            
+            it("emits an Transfer event", async() =>{
+                const log = result.logs[0]
+                log.event.should.equal('Approval')
+                const event = log.args
+                event.owner.toString().should.equal(accounts[0],"Owner is correct")
+                event.spender.toString().should.equal(accounts[2].toString(),"Spender is correct")
+                event.value.toString().should.equal(amount.toString(),"Value is correct")
+            })
+        })
+        describe('failure', () =>{
+            it('rejects invalid spender', async() =>{
+                await token.approve(0x0, amount,{from: accounts[0]}).should.be.rejected
+            })
+        })
+    })
+
+
+    //Test for exchange token transfer function i.e. transFrom 
+    describe('sending tokens by exchange (accounts[2]) to different account i.e. delegated token transfer',  () =>{
+        let result
+        let transferAmount = tokens(200)
+        let amount
+
+        beforeEach(async () =>{
+            amount = tokens(100)
+            await token.transfer(accounts[1],transferAmount, {from: accounts[0]})
+            await token.approve(accounts[0], amount, {from: accounts[1]})   // approving for exchange to transfer tokens from accounts[1] for all test cases
+        })
+
+        // All test cases for successfull token transfer
+        describe('Successful token transfer', async () =>{
+            beforeEach(async()=>{
+              //Transfer
+            
+            result = await token.transferFrom(accounts[1],accounts[2],amount, {from: accounts[0]}) 
+           })
+
+          it('transfer token balance', async () =>{
+            let balanceOf
+        //After Transfer
+            balanceOf = await token.balanceOf(accounts[1])
+            balanceOf.toString().should.equal(tokens(999700).toString())
+            balanceOf = await token.balanceOf(accounts[2])
+            balanceOf.toString().should.equal(tokens(100).toString())
+          })  
+
+          it('reset the allowance', async () =>{
+            const allowance = await token.allowance(accounts[1],accounts[2]) 
+            allowance.toString().should.equal('0') // checking if the amount of tokens are added to the allowance
+        })
+    
+        //   Test for TransferF event with details of the sender , reciever and the amount of tokens send in transcation
+          it('emits a transfer event', async () =>{
+              const logs = result.logs[0]  // We get logs array in return when function called. We are extracting that logs
+              logs.event.should.equal('Transfer')  // Comparing that right event is triggered
+              const event = logs.args    // args is array which contain details about the transcation happened
+              event.from.toString().should.equal(accounts[1], 'Sender is correct')
+              event.to.toString().should.equal(accounts[2], 'reciever is correct')
+              event.value.toString().should.equal(amount.toString(), 'Value is correct')
+          })
+
+        })
+        
+        //All test cases when token failed to transfer
+        describe('Failure in token transfer', async () =>{
+            it('rejects insufficient balance and cant send more than approved tokens',async()=>{
+                let invalidAmount
+                //Attempting to send tokens more then user have
+                invalidAmount = tokens(1000000000) // 100 million - IT is greator then total supply or sender's or deployer's account
+                await token.transferFrom(accounts[1],accounts[2],invalidAmount, {from: accounts[0]}).should.be.rejectedWith(EVM_REVERT)
+               
+            })
+           
+            it('Sending token to invalid account', async() =>{
+                await token.transferFrom(accounts[1],0x0, {from: accounts[0]}).should.be.rejected
+            })
+        })
+    })
 })
